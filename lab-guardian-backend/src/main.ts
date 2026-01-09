5f28cc23e9c5d268092ad03ca9decc02cb6a72cb
thisnorm <kisook2557@gmail.com>
2026-01-09 14:12:52 +0900

    feat : 백엔드 -> 웹 포트 허용

--- lab-guardian-backend/src/main.ts
+++ lab-guardian-backend/src/main.ts
@@ -1,15 +1,18 @@
-// src/main.ts
+// lab-guardian-backend/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// lab-guardian-backend/src/main.ts
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // CORS 허용 (React 5000포트에서 접속 가능하도록)
-  app.enableCors();
-  
-  // 포트 8000번 사용
-  await app.listen(8000);
-  console.log(`🚀 NestJS Backend is running on: http://192.168.0.131:8000`);
+
+  app.enableCors({
+    origin: true, // 요청이 들어오는 도메인을 자동으로 허용
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(8000, '0.0.0.0'); // 모든 IP 접속 허용
+  console.log(`🚀 NestJS Hub running on: http://192.168.0.131:8000`);
 }
 bootstrap();
