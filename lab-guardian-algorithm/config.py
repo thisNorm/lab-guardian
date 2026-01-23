5ed266d331ca680179258f6fb335d7f85a31e742
thisnorm <kisook2557@gmail.com>
2026-01-23 16:38:38 +0900

    fix : PC_IP 주소 변경 및 리얼센스 사용 트루로 변경

--- lab-guardian-algorithm/config.py
+++ lab-guardian-algorithm/config.py
@@ -1,6 +1,6 @@
 # config.py
 MY_IP = "192.168.0.100"      # 라즈베리파이 IP
-PC_IP = "172.20.10.2"        # 데스크탑 IP (NestJS, React, Algo 실행처)
+PC_IP = "192.168.0.149"      # 데스크탑 IP (NestJS, React, Algo 실행처)
 
 PORT_ALGO = 3000
 PORT_NEST = 8000
@@ -10,4 +10,4 @@ PORT_ROBOT = 9999
 NEST_API_URL = f"http://{PC_IP}:{PORT_NEST}"
 
 # 장치 옵션
-USE_REALSENSE = False  # RealSense 카메라를 사용할 경우 True로 변경
+USE_REALSENSE = True  # RealSense 카메라를 사용할 경우 True로 변경
